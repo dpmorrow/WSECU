a859fe7550ec0a32d2f5e7422dea2c0137cfd322
Douglas Morrow <douglas.p.morrow@gmail.com>
2016-12-11 23:49:41 -0800

    add user-search-input tests and made "Search Term" element a label

--- src/app/user-search/user-search-input/user-search-input.component.spec.ts
+++ src/app/user-search/user-search-input/user-search-input.component.spec.ts
@@ -4,10 +4,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserSearchInputComponent } from './user-search-input.component';
 import {UserSearchService} from "../user-search-service/user-search-service";
 import {UserSearchServiceFake} from "../user-search-service/user-search-service.fake";
+import {By} from "@angular/platform-browser";
+import {click} from "../../../test-helpers/click";
 
 describe('UserSearchInputComponent', () => {
   let component: UserSearchInputComponent;
   let fixture: ComponentFixture<UserSearchInputComponent>;
+  let searchButtonEl;
+  let searchInputEl;
+  let expectedTerm = 'term';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,10 +27,19 @@ describe('UserSearchInputComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserSearchInputComponent);
     component = fixture.componentInstance;
+    searchInputEl = fixture.debugElement.query(By.css('.user-search-input'));
+    searchButtonEl = fixture.debugElement.query(By.css('.user-search-button'));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call doSearch when search button is clicked', () => {
+    searchInputEl.nativeElement.value = expectedTerm;
+    spyOn(component, 'doSearch');
+    click(searchButtonEl);
+    expect(component.doSearch).toHaveBeenCalledWith(expectedTerm);
   });
 });
