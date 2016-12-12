9ac61b1640463e7a6bac5211208309990da7057a
Douglas Morrow <douglas.p.morrow@gmail.com>
2016-12-11 23:29:35 -0800

    add tests using MockBackend for UserSearchService

--- src/app/user-search/user-search-service/user-search-service.spec.ts
+++ src/app/user-search/user-search-service/user-search-service.spec.ts
@@ -2,19 +2,64 @@
 
 import { TestBed, inject } from '@angular/core/testing';
 import { UserSearchService } from './user-search-service';
-import {HttpModule} from "@angular/http";
+import {HttpModule, BaseRequestOptions, Http, Response, ResponseOptions} from "@angular/http";
+import {MockBackend, MockConnection} from "@angular/http/testing";
 
 describe('UserSearchService', () => {
+
+  function setupMockBackend(mockBackend: MockBackend) {
+    mockBackend.connections.subscribe(
+      (connection: MockConnection) => {
+        connection.mockRespond(new Response(
+          new ResponseOptions({
+            body: require('./user-search-service-mock-response.json')
+          })
+        ))
+      }
+    )
+  }
+
   beforeEach(() => {
+    //noinspection JSUnusedGlobalSymbols
     TestBed.configureTestingModule({
-      providers: [UserSearchService],
+      providers: [
+        UserSearchService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend, options) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ],
       imports: [
         HttpModule
       ]
     });
   });
 
-  it('should ...', inject([UserSearchService], (service: UserSearchService) => {
+  it('should create', inject([UserSearchService], (service: UserSearchService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should provide subscribers a list of users', inject([UserSearchService, MockBackend], (service: UserSearchService, mockBackend) => {
+    setupMockBackend(mockBackend);
+
+    service.initialize();
+
+    service.userResults$.subscribe(userResults => {
+      expect(userResults.length).toBeGreaterThan(0);
+      expect(typeof userResults).toBe('Array<User>');
+    });
+  }));
+
+  it('should provide subscribers the search term after a search', inject([UserSearchService], (service: UserSearchService) => {
+    let expectedTerm = 'term';
+    service.executeSearch(expectedTerm);
+
+    service.searchTerm$.subscribe(searchTerm => {
+      expect(searchTerm).toBe(expectedTerm);
+    });
+  }));
+
 });
